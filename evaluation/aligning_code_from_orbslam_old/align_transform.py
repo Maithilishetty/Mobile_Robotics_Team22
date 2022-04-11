90bc6c7e58ceed6943067be5c93d7976c5860e96
liuxy10 <liuxytxdy10@gmail.com>
2022-04-11 08:38:26 -0400

    Added data and evaluation

---
+++ evaluation/aligning_code_from_orbslam_old/align_transform.py
@@ -0,0 +1,60 @@
+
+import sys
+import numpy
+import argparse
+import associate
+from evaluate_ate_scale import *
+
+if __name__=="__main__":
+    # parse command line
+    parser = argparse.ArgumentParser(description='''
+    This script computes the absolute trajectory error from the ground truth trajectory and the estimated trajectory. 
+    ''')
+    parser.add_argument('first_file', help='ground truth trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('second_file', help='estimated trajectory (format: timestamp tx ty tz qx qy qz qw)')
+    parser.add_argument('--offset', help='time offset added to the timestamps of the second file (default: 0.0)',default=0.0)
+    parser.add_argument('--scale', help='scaling factor for the second trajectory (default: 1.0)',default=1.0)
+    parser.add_argument('--max_difference', help='maximally allowed time difference for matching entries (default: 10000000 ns)',default=20000000)
+    parser.add_argument('--save', help='save aligned second trajectory to disk (format: stamp2 x2 y2 z2)')
+    parser.add_argument('--save_associations', help='save associated first and aligned second trajectory to disk (format: stamp1 x1 y1 z1 stamp2 x2 y2 z2)')
+    parser.add_argument('--plot', help='plot the first and the aligned second trajectory to an image (format: png)')
+    parser.add_argument('--verbose', help='print all evaluation data (otherwise, only the RMSE absolute translational error in meters after alignment will be printed)', action='store_true')
+    parser.add_argument('--verbose2', help='print scale eror and RMSE absolute translational error in meters after alignment with and without scale correction', action='store_true')
+    args = parser.parse_args()
+
+
+    first_list = associate.read_file_list(args.first_file, False)
+    second_list = associate.read_file_list(args.second_file, False)
+
+    matches = associate.associate(first_list, second_list,float(args.offset),float(args.max_difference))   #??
+    if len(matches)<2:
+        sys.exit("Couldn't find matching timestamp pairs between groundtruth and estimated trajectory! Did you choose the correct sequence?")
+    first_xyz = numpy.matrix([[float(value) for value in first_list[a][0:3]] for a,b in matches]).transpose()
+    second_xyz = numpy.matrix([[float(value)*float(args.scale) for value in second_list[b][0:3]] for a,b in matches]).transpose()
+    dictionary_items = second_list.items()
+    sorted_second_list = sorted(dictionary_items)
+
+    second_xyz_full = numpy.matrix([[float(value)*float(args.scale) for value in sorted_second_list[i][1][0:3]] for i in range(len(sorted_second_list))]).transpose() # sorted_second_list.keys()]).transpose()
+    rot,transGT,trans_errorGT,trans,trans_error, scale = align(second_xyz,first_xyz) # TBH I don't think there should be a scale term...
+
+    second_xyz_aligned = scale * rot * second_xyz + trans
+    second_xyz_notscaled = rot * second_xyz + trans
+    second_xyz_notscaled_full = rot * second_xyz_full + trans
+    first_stamps = first_list.keys()
+    first_stamps.sort()
+    first_xyz_full = numpy.matrix([[float(value) for value in first_list[b][0:3]] for b in first_stamps]).transpose()
+    
+    second_stamps = second_list.keys()
+    second_stamps.sort()
+    second_xyz_full = numpy.matrix([[float(value)*float(args.scale) for value in second_list[b][0:3]] for b in second_stamps]).transpose()
+    second_xyz_full_aligned = scale * rot * second_xyz_full + trans
+
+    
+
+
+
+    
+
+
+
+
